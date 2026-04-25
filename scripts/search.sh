d8316a2e62b07eb223edacd5a90d3580d4e9eba3
MikeMoolenaar <mmoolenaar9@gmail.com>
2026-04-25 13:40:08 +0200

    Split hyprland config in 2 configs

--- scripts/search.sh
+++
@@ -1,5 +0,0 @@
-YDOTOOL_SOCKET="$HOME/.ydotool_socket" ydotool click 0xC2 # Middle button
-CLIPBOARD=$(wl-paste -p)
-xdotool search --onlyvisible --name firefox windowactivate
-firefox --new-tab "https://duckduckgo.com/?q=$CLIPBOARD"
-
